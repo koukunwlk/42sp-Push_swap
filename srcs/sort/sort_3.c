22fe488c8fcd5a670c71ccf627ffd5b711fbf289
Moacir Amaro <moacir@gmail.com>
2022-02-03 14:46:44 -0300

    fix sort_3

--- srcs/sort/sort_3.c
+++ srcs/sort/sort_3.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:53:59 by mamaro-d          #+#    #+#             */
-/*   Updated: 2022/02/03 14:02:23 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2022/02/03 14:45:35 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 
 void	sort_3(t_stack **stack_a)
 {
-	if(is_sorted(*stack_a))
+	if (is_sorted(*stack_a))
 		return ;
-	if ((*stack_a)->data > (*stack_a)->next->data)
+	if ((*stack_a)->data > (*stack_a)->next->data && (*stack_a)->data < (*stack_a)->next->next->data)
 		sa_op(stack_a, 0);
-	if ((*stack_a)->data > (*stack_a)->next->next->data)
+	else if ((*stack_a)->data > (*stack_a)->next->data && (*stack_a)->next->data > (*stack_a)->next->next->data)
+		sa_op(stack_a, 0);
+	else if ((*stack_a)->data > (*stack_a)->next->data && (*stack_a)->next->data > (*stack_a)->next->next->data)
+		ra_op(stack_a, 0);
+	else if ((*stack_a)->data > (*stack_a)->next->data && (*stack_a)->next->data < (*stack_a)->next->next->data)
+		ra_op(stack_a, 0);
+	else if ((*stack_a)->data < (*stack_a)->next->data && (*stack_a)->next->data > (*stack_a)->next->next->data)
 		rra_op(stack_a, 0);
 	sort_3(stack_a);
 }
