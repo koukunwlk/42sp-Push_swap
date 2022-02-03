f81a498e6fb84b88db34874728238046ba4a6772
Moacir Amaro <moacir@gmail.com>
2022-02-03 14:02:41 -0300

    fix sort_3

--- srcs/sort/sort_3.c
+++ srcs/sort/sort_3.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:53:59 by mamaro-d          #+#    #+#             */
-/*   Updated: 2022/02/03 13:46:05 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2022/02/03 14:02:23 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	sort_3(t_stack **stack_a)
 {
 	if(is_sorted(*stack_a))
 		return ;
-	if((*stack_a)->data > (*stack_a)->next->data && (*stack_a)->next->data < (*stack_a)->next->next->data)
+	if ((*stack_a)->data > (*stack_a)->next->data)
 		sa_op(stack_a, 0);
-	else if((*stack_a)->data < (*stack_a)->next->data && (*stack_a)->next->data > (*stack_a)->next->next->data)
+	if ((*stack_a)->data > (*stack_a)->next->next->data)
 		rra_op(stack_a, 0);
 	sort_3(stack_a);
 }
