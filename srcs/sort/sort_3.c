503023eade1d1140672f92ef9314dccec16f91e6
Moacir Amaro <moacir@gmail.com>
2022-02-03 15:09:11 -0300

    fix sort_3

--- srcs/sort/sort_3.c
+++ srcs/sort/sort_3.c
@@ -6,14 +6,14 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:53:59 by mamaro-d          #+#    #+#             */
-/*   Updated: 2022/02/03 14:45:35 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2022/02/03 15:08:54 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 
-void	sort_3(t_stack **stack_a)
+/* void	sort_3(t_stack **stack_a)
 {
 	if (is_sorted(*stack_a))
 		return ;
@@ -28,4 +28,34 @@ void	sort_3(t_stack **stack_a)
 	else if ((*stack_a)->data < (*stack_a)->next->data && (*stack_a)->next->data > (*stack_a)->next->next->data)
 		rra_op(stack_a, 0);
 	sort_3(stack_a);
+} */
+
+void	sort_3(t_stack **stack_a)
+{
+	if ((*stack_a)->data > (*stack_a)->next->data && (*stack_a)->next->data
+		< (*stack_a)->next->next->data && (*stack_a)->next->next->data
+		< (*stack_a)->data)
+		sa_op(stack_a, 0);
+	else if ((*stack_a)->data < (*stack_a)->next->data && (*stack_a)->next->data
+			< (*stack_a)->next->next->data && (*stack_a)->next->next->data
+			> (*stack_a)->data)
+	{
+		sa_op(stack_a, 0);
+		rra_op(stack_a, 0);
+	}
+	else if ((*stack_a)->data > (*stack_a)->next->data && (*stack_a)->next->data
+			< (*stack_a)->next->next->data && (*stack_a)->next->next->data
+			> (*stack_a)->data)
+		ra_op(stack_a, 0);
+	else if ((*stack_a)->data < (*stack_a)->next->data && (*stack_a)->next->data
+			> (*stack_a)->next->next->data && (*stack_a)->next->next->data
+			< (*stack_a)->data)
+	{
+		sa_op(stack_a, 0);
+		ra_op(stack_a, 0);
+	}
+	else if ((*stack_a)->data < (*stack_a)->next->data && (*stack_a)->next->data
+			> (*stack_a)->next->next->data && (*stack_a)->next->next->data
+			> (*stack_a)->data)
+	 	rra_op(stack_a, 0);
 }
