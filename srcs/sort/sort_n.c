42a0247751af15c55dee45568e7aacab08962a16
Moacir Amaro <moacir@gmail.com>
2022-02-11 16:02:55 -0300

    test solve

--- srcs/sort/sort_n.c
+++ srcs/sort/sort_n.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 15:47:03 by mamaro-d          #+#    #+#             */
-/*   Updated: 2022/02/11 16:01:21 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2022/02/11 16:02:43 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,14 @@ void	sort_n(t_data *data)
 	data->group_count = group_count(data);
 	data->group_size = stack_size(data->a) / data->group_count;
 	group = 1;
-	while(group < data->group_count + 1)
+	while(data->a)
 	{
 		nearest_in_group = find_nearest_in_group(data->a, group, data->group_size);
 		move_to_b(data, nearest_in_group);
 		if(nearest_in_group == NULL)
 			group++;
 	}
+	
 }
 
 t_stack	*find_nearest_in_group(t_stack *stack, int group, int group_size)
